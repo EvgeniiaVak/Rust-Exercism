512686f4656013e733c64f2a90491caf21383465
Evgeniia Vakarina <27793901+EvgeniiaVak@users.noreply.github.com>
2022-12-13 19:22:58 +0400

    wip(sublist): fail to implement with std::thread

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -1,9 +1,47 @@
+use std::marker::Sync;
+use std::sync::mpsc;
+use std::thread;
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum Comparison {
     Equal,
     Sublist,
     Superlist,
     Unequal,
+}
+
+fn is_superlist<T: PartialEq>(a: &[T], b: &[T]) -> bool {
+    a.windows(b.len()).any(|w| w == b)
+}
+
+fn is_superlist_threads<T: PartialEq + Sync>(a: &[T], b: &[T]) -> bool {
+    let (tx, rx) = mpsc::channel();
+
+    let windows = a.windows(b.len()).collect::<Vec<&[T]>>();
+
+    for window in windows {
+        // create a new transmitter for each thread
+        let tx = tx.clone();
+
+        // TODO: is this clone necessary?
+        let window = window.clone();
+        let b = b.clone();
+
+        // FIXME: error[E0521]: borrowed data escapes outside of function
+        thread::spawn(move || {
+            let are_equal = window == b;
+            tx.send(are_equal).unwrap();
+        });
+    }
+
+    for received in rx {
+        if received {
+            // TODO: kill all other threads?
+            return true;
+        }
+    }
+
+    false
 }
 
 pub fn sublist<T: PartialEq>(a: &[T], b: &[T]) -> Comparison {
@@ -19,9 +57,10 @@ pub fn sublist<T: PartialEq>(a: &[T], b: &[T]) -> Comparison {
         return Comparison::Superlist;
     }
 
-    let is_superlist = a.windows(b.len()).any(|w| w == b);
-    let is_sublist = b.windows(a.len()).any(|w| w == a);
-    match (is_sublist, is_superlist) {
+    let superlist = is_superlist(a, b);
+    let sublist = is_superlist(b, a);
+
+    match (sublist, superlist) {
         (true, true) => Comparison::Equal,
         (true, false) => Comparison::Sublist,
         (false, true) => Comparison::Superlist,
