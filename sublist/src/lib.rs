c5dea09ab6fa3cd07200c08292821cd01d27873e
Evgeniia Vakarina <27793901+EvgeniiaVak@users.noreply.github.com>
2022-12-17 23:45:30 +0400

    fix(sublist): scoped threads not compiling
    Now the method runs for ever.

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -12,39 +12,38 @@ pub enum Comparison {
     Unequal,
 }
 
+#[allow(dead_code)]
 fn is_superlist<T: PartialEq>(a: &[T], b: &[T]) -> bool {
     a.windows(b.len()).any(|w| w == b)
 }
 
+#[allow(dead_code)]
 fn is_superlist_rayon<T: PartialEq + Sync>(a: &[T], b: &[T]) -> bool {
     //! Parallelize the window comparisons using rayon.
 
     a.par_windows(b.len()).any(|w| w == b)
 }
 
-fn is_superlist_threads<T: PartialEq + Sync>(a: &[T], b: &[T]) -> bool {
+fn is_superlist_threads<T: PartialEq + Sync + Send>(a: &[T], b: &[T]) -> bool {
     //! Strateforwardly send each window comparison to a new thread (no pooling).
 
     let a = Arc::new(a);
     let b = Arc::new(b);
 
     // scoped threads as in https://stackoverflow.com/a/32751956/9076659
-    thread::scope(|_| {
+    thread::scope(|s| {
         let (tx, rx) = mpsc::channel();
 
         for window in a.windows(b.len()) {
             // create a new transmitter for each thread
             let tx = tx.clone();
 
-            let b = *b.clone();
-            let window = window.clone();
+            let b = b.clone();
 
-            // FIXME: error[E0521]: borrowed data escapes outside of function
-            // maybe relevant https://users.rust-lang.org/t/why-does-thread-spawn-need-static-lifetime-for-generic-bounds/4541
-            // thread::spawn(move || {
-            //     let are_equal = window == b;
-            //     tx.send(are_equal);
-            // });
+            s.spawn(move || {
+                let are_equal = *b == window;
+                tx.send(are_equal);
+            });
         }
 
         for received in rx {
@@ -58,7 +57,7 @@ fn is_superlist_threads<T: PartialEq + Sync>(a: &[T], b: &[T]) -> bool {
     }) // no semicolon to return result
 }
 
-pub fn sublist<T: PartialEq + Sync>(a: &[T], b: &[T]) -> Comparison {
+pub fn sublist<T: PartialEq + Sync + Send>(a: &[T], b: &[T]) -> Comparison {
     if a == b {
         return Comparison::Equal;
     }
@@ -71,8 +70,8 @@ pub fn sublist<T: PartialEq + Sync>(a: &[T], b: &[T]) -> Comparison {
         return Comparison::Superlist;
     }
 
-    let superlist = is_superlist_rayon(a, b);
-    let sublist = is_superlist_rayon(b, a);
+    let superlist = is_superlist_threads(a, b);
+    let sublist = is_superlist_threads(b, a);
 
     match (sublist, superlist) {
         (true, true) => Comparison::Equal,
