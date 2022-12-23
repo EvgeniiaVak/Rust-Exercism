1dac47d9dbd1dacbb47780570b528e0f64fc7e1d
Evgeniia Vakarina <27793901+EvgeniiaVak@users.noreply.github.com>
2022-12-23 14:31:48 +0400

    wip(sublist): failed threads with task queue

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -1,7 +1,8 @@
 use rayon::prelude::*;
+use std::collections::VecDeque;
 use std::marker::Sync;
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::{mpsc, Arc, Mutex};
+use std::sync::{Arc, Mutex};
 use std::thread::{self, available_parallelism};
 
 #[derive(Debug, PartialEq, Eq)]
@@ -24,148 +25,50 @@ pub fn is_superlist_rayon<T: PartialEq + Sync>(a: &[T], b: &[T]) -> bool {
     a.par_windows(b.len()).any(|w| w == b)
 }
 
-#[allow(dead_code)]
+type Job = Box<dyn FnOnce() + Send>;
+
 pub fn is_superlist_threads<T: PartialEq + Sync>(a: &[T], b: &[T]) -> bool {
-    //! Strateforwardly send each window comparison to a new thread (no pooling).
+    //! Create threads and task queue to parallelize the window comparisons.
 
-    let a = Arc::new(a);
     let b = Arc::new(b);
-
-    // scoped threads as in https://stackoverflow.com/a/32751956/9076659
-    thread::scope(|s| {
-        let (tx, rx) = mpsc::channel();
-        let mut thread_counter = 0;
-
-        for window in a.windows(b.len()) {
-            thread_counter += 1;
-            // create a new transmitter for each thread
-            let tx = tx.clone();
-
-            let b = b.clone();
-
-            s.spawn(move || {
-                // TODO: kill the thread if we already found a match
-                let are_equal = *b == window;
-                tx.send(are_equal).unwrap();
-            });
-        }
-
-        for _ in 0..thread_counter {
-            if rx.recv() == Ok(true) {
-                return true;
-            }
-        }
-
-        false
-    }) // no semicolon to return result
-}
-
-type Job = Box<dyn FnOnce() + Send + 'static>;
-
-struct Worker<'s> {
-    id: usize,
-    thread: thread::ScopedJoinHandle<'s, ()>,
-}
-
-impl<'s> Worker<'s> {
-    fn new(
-        id: usize,
-        receiver: Arc<Mutex<mpsc::Receiver<Job>>>,
-        scope: &'s thread::Scope<'s, '_>,
-    ) -> Worker<'s> {
-        let thread = scope.spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
-
-            println!("Worker {} got a job; executing.", id);
-
-            job();
-        });
-
-        Worker { id, thread }
-    }
-}
-
-pub struct ScopedThreadPool<'s> {
-    max_threads: usize,
-    workers: Vec<Worker<'s>>,
-    sender: mpsc::Sender<Job>,
-}
-
-impl<'s> ScopedThreadPool<'s> {
-    /// Create a new ThreadPool.
-    ///
-    /// The size is the number of threads in the pool.
-    ///
-    /// # Panics
-    ///
-    /// The `new` function will panic if the size is zero.
-    pub fn new(size: usize, scope: &'s thread::Scope<'s, '_>) -> ScopedThreadPool<'s> {
-        assert!(size > 0);
-
-        let mut workers = Vec::with_capacity(size);
-        let (sender, receiver) = mpsc::channel();
-
-        let receiver = Arc::new(Mutex::new(receiver));
-
-        for i in 0..size {
-            workers.push(Worker::new(i, Arc::clone(&receiver), scope));
-        }
-
-        ScopedThreadPool {
-            max_threads: size,
-            workers,
-            sender,
-        }
-    }
-
-    fn execute<F: FnOnce() + Send + 'static>(&mut self, f: F) {
-        let job = Box::new(f);
-
-        self.sender.send(job).unwrap();
-    }
-}
-
-fn is_superlist_threadpool<T: PartialEq + Sync>(a: &[T], b: &[T]) -> bool {
-    //! Execute window comparisons within a thread pool.
-    //!
-    //! Based on https://doc.rust-lang.org/book/ch20-02-multithreaded.html#improving-throughput-with-a-thread-pool
-
-    let a = Arc::new(a);
-    let b = Arc::new(b);
-
-    let found_match = Arc::new(AtomicBool::new(false));
-
     let num_threads = match available_parallelism() {
         Ok(n) => n.get(),
         Err(_) => 4 as usize,
     };
 
-    thread::scope(|scope| {
-        let mut pool = ScopedThreadPool::new(num_threads, scope);
+    // FIXME: lifetime may not live long enough
+    let tasks: Arc<Mutex<VecDeque<Job>>> = Arc::new(Mutex::new(VecDeque::new()));
+    let found_match = Arc::new(AtomicBool::new(false));
 
-        for window in a.windows(b.len()) {
-            // exit early if we've already found a match
-            if found_match.load(Ordering::Relaxed) {
-                return;
+    for window in a.windows(b.len()) {
+        let b = b.clone();
+        let found_match = Arc::clone(&found_match);
+
+        tasks.lock().unwrap().push_back(Box::new(move || {
+            if *b == window {
+                found_match.store(true, Ordering::Relaxed);
             };
+        }));
+    }
 
-            let found_match = Arc::clone(&found_match);
-            let b = Arc::clone(&b);
+    for _ in 0..num_threads {
+        let tasks = Arc::clone(&tasks);
+        let found_match = Arc::clone(&found_match);
 
-            // FIXME: borrowed data escapes outside of function
-            pool.execute(move || {
-                // exit early if we've already found a match
-                if found_match.load(Ordering::Relaxed) {
-                    return;
-                };
-                if *b == window {
-                    found_match.store(true, Ordering::Relaxed);
+        thread::spawn(move || loop {
+            let task = tasks.lock().unwrap().pop_front();
+
+            match task {
+                Some(task) => {
+                    task();
+                    if found_match.load(Ordering::Relaxed) {
+                        break;
+                    }
                 }
-            });
-        }
-
-        // TODO: wait for all jobs to finish and shut down the thread pool
-    });
+                None => break,
+            }
+        });
+    }
 
     found_match.load(Ordering::Relaxed)
 }
@@ -183,8 +86,8 @@ pub fn sublist<T: PartialEq + Sync>(a: &[T], b: &[T]) -> Comparison {
         return Comparison::Superlist;
     }
 
-    let superlist = is_superlist_threadpool(a, b);
-    let sublist = is_superlist_threadpool(b, a);
+    let superlist = is_superlist_threads(a, b);
+    let sublist = is_superlist_threads(b, a);
 
     match (sublist, superlist) {
         (true, true) => Comparison::Equal,
