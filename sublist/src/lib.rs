67b1bc91a79bdc1d24ea5de98484004ca8020124
Evgeniia Vakarina <27793901+EvgeniiaVak@users.noreply.github.com>
2022-12-28 22:59:00 +0400

    style(sublist): make the code more readable

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -5,6 +5,31 @@ use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{Arc, Mutex};
 use std::thread::{self, available_parallelism};
 
+// https://exercism.org/tracks/rust/exercises/sublist
+pub fn sublist<T: PartialEq + Sync>(a: &[T], b: &[T]) -> Comparison {
+    if a == b {
+        return Comparison::Equal;
+    }
+
+    if a.is_empty() {
+        return Comparison::Sublist;
+    }
+
+    if b.is_empty() {
+        return Comparison::Superlist;
+    }
+
+    let superlist = is_superlist_threads(a, b);
+    let sublist = is_superlist_threads(b, a);
+
+    match (sublist, superlist) {
+        (true, true) => Comparison::Equal,
+        (true, false) => Comparison::Sublist,
+        (false, true) => Comparison::Superlist,
+        (false, false) => Comparison::Unequal,
+    }
+}
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum Comparison {
     Equal,
@@ -28,7 +53,7 @@ pub fn is_superlist_rayon<T: PartialEq + Sync>(a: &[T], b: &[T]) -> bool {
 }
 
 pub fn is_superlist_threads<T: PartialEq + Sync>(a: &[T], b: &[T]) -> bool {
-    //! Create threads and task queue to parallelize the window comparisons.
+    //! Parallelize the window comparisons using std-only tools.
 
     let b = Arc::new(b);
     let num_threads = match available_parallelism() {
@@ -36,23 +61,25 @@ pub fn is_superlist_threads<T: PartialEq + Sync>(a: &[T], b: &[T]) -> bool {
         Err(_) => 4 as usize,
     };
 
-    let jobs = Arc::new(Mutex::new(VecDeque::new()));
+    let tasks = Arc::new(Mutex::new(VecDeque::new()));
     let found_match = Arc::new(AtomicBool::new(false));
 
+    // create the tasks and put them in one queue
     for window in a.windows(b.len()) {
         let b = b.clone();
         let found_match = Arc::clone(&found_match);
 
-        jobs.lock().unwrap().push_back(move || {
+        tasks.lock().unwrap().push_back(move || {
             if *b == window {
                 found_match.store(true, Ordering::Relaxed);
             };
         });
     }
 
+    // spawn the threads and give them the tasks
     thread::scope(|s| {
         for _ in 0..num_threads {
-            let tasks = Arc::clone(&jobs);
+            let tasks = Arc::clone(&tasks);
             let found_match = Arc::clone(&found_match);
 
             s.spawn(move || loop {
@@ -72,28 +99,4 @@ pub fn is_superlist_threads<T: PartialEq + Sync>(a: &[T], b: &[T]) -> bool {
     });
 
     found_match.load(Ordering::Relaxed)
-}
-
-pub fn sublist<T: PartialEq + Sync>(a: &[T], b: &[T]) -> Comparison {
-    if a == b {
-        return Comparison::Equal;
-    }
-
-    if a.is_empty() {
-        return Comparison::Sublist;
-    }
-
-    if b.is_empty() {
-        return Comparison::Superlist;
-    }
-
-    let superlist = is_superlist_threads(a, b);
-    let sublist = is_superlist_threads(b, a);
-
-    match (sublist, superlist) {
-        (true, true) => Comparison::Equal,
-        (true, false) => Comparison::Sublist,
-        (false, true) => Comparison::Superlist,
-        (false, false) => Comparison::Unequal,
-    }
 }
