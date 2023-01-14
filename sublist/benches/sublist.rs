69ee821f97438255d2ec0524add554727ad3c9f2
Evgeniia Vakarina <27793901+EvgeniiaVak@users.noreply.github.com>
2023-01-14 14:01:14 +0400

    refactor(sublist): move profiling to .idea

--- sublist/benches/sublist.rs
+++ sublist/benches/sublist.rs
@@ -1,9 +1,6 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use rand::{distributions::Alphanumeric, thread_rng, Rng};
-use std::fs;
 use sublist::{sublist, Method};
-
-use pprof::criterion::{Output, PProfProfiler};
 
 fn bench_sublist(c: &mut Criterion) {
     let mut group = c.benchmark_group("Sublist");
@@ -35,11 +32,7 @@ fn bench_sublist(c: &mut Criterion) {
 
 criterion_group!(
     name = benches;
-    // FIXME: this fails on macos
-    // command: cargo bench --bench sublist -- --profile-time=5
-    // error: process didn't exit successfully: ... (signal: 10, SIGBUS: access to undefined memory)
-    // sometimes it finishes, but the results look weird
-    config = Criterion::default().with_profiler(PProfProfiler::new(1, Output::Flamegraph(None)));
+    config = Criterion::default();
     targets = bench_sublist
 );
 criterion_main!(benches);
--- sublist/tests/sublist.rs
+++ sublist/tests/sublist.rs
@@ -1,9 +1,47 @@
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
 use sublist::{sublist, Comparison, Method};
 
 fn test_sublist<T: PartialEq + Sync>(expected_comparison: Comparison, a: &[T], b: &[T]) {
     assert_eq!(expected_comparison, sublist(a, b, Method::Sequential));
     assert_eq!(expected_comparison, sublist(a, b, Method::Rayon));
     assert_eq!(expected_comparison, sublist(a, b, Method::Threads));
+}
+
+fn test_large_sublist(method: Method) {
+    let i = 100_000_000;
+
+    // random chars
+    let rand = |i| {
+        thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(i)
+            .map(char::from)
+            .collect::<Vec<char>>()
+    };
+
+    let a = rand(i);
+    let b = rand(i / 4);
+
+    sublist(&a, &b, method);
+}
+
+#[test]
+#[ignore]
+fn large_threads() {
+    test_large_sublist(Method::Threads);
+}
+
+#[test]
+#[ignore]
+fn large_rayon() {
+    test_large_sublist(Method::Rayon);
+}
+
+#[test]
+#[ignore]
+fn large_sequential() {
+    test_large_sublist(Method::Sequential);
 }
 
 #[test]
@@ -14,25 +52,21 @@ fn empty_equals_empty() {
 }
 
 #[test]
-#[ignore]
 fn test_empty_is_a_sublist_of_anything() {
     test_sublist(Comparison::Sublist, &[], &['a', 's', 'd', 'f']);
 }
 
 #[test]
-#[ignore]
 fn test_anything_is_a_superlist_of_empty() {
     test_sublist(Comparison::Superlist, &['a', 's', 'd', 'f'], &[]);
 }
 
 #[test]
-#[ignore]
 fn test_1_is_not_2() {
     test_sublist(Comparison::Unequal, &[1], &[2]);
 }
 
 #[test]
-#[ignore]
 fn test_compare_larger_equal_lists() {
     use std::iter::repeat;
 
@@ -42,31 +76,26 @@ fn test_compare_larger_equal_lists() {
 }
 
 #[test]
-#[ignore]
 fn test_sublist_at_start() {
     test_sublist(Comparison::Sublist, &[1, 2, 3], &[1, 2, 3, 4, 5]);
 }
 
 #[test]
-#[ignore]
 fn sublist_in_middle() {
     test_sublist(Comparison::Sublist, &[4, 3, 2], &[5, 4, 3, 2, 1]);
 }
 
 #[test]
-#[ignore]
 fn sublist_at_end() {
     test_sublist(Comparison::Sublist, &[3, 4, 5], &[1, 2, 3, 4, 5]);
 }
 
 #[test]
-#[ignore]
 fn partially_matching_sublist_at_start() {
     test_sublist(Comparison::Sublist, &[1, 1, 2], &[1, 1, 1, 2]);
 }
 
 #[test]
-#[ignore]
 fn sublist_early_in_huge_list() {
     let huge: Vec<u32> = (1..1_000_000).collect();
 
@@ -74,7 +103,6 @@ fn sublist_early_in_huge_list() {
 }
 
 #[test]
-#[ignore]
 fn huge_sublist_not_in_huge_list() {
     let v1: Vec<u64> = (10..1_000_001).collect();
     let v2: Vec<u64> = (1..1_000_000).collect();
@@ -83,31 +111,26 @@ fn huge_sublist_not_in_huge_list() {
 }
 
 #[test]
-#[ignore]
 fn superlist_at_start() {
     test_sublist(Comparison::Superlist, &[1, 2, 3, 4, 5], &[1, 2, 3]);
 }
 
 #[test]
-#[ignore]
 fn superlist_in_middle() {
     test_sublist(Comparison::Superlist, &[5, 4, 3, 2, 1], &[4, 3, 2]);
 }
 
 #[test]
-#[ignore]
 fn superlist_at_end() {
     test_sublist(Comparison::Superlist, &[1, 2, 3, 4, 5], &[3, 4, 5]);
 }
 
 #[test]
-#[ignore]
 fn second_list_missing_element_from_first_list() {
     test_sublist(Comparison::Unequal, &[1, 2, 3], &[1, 3]);
 }
 
 #[test]
-#[ignore]
 fn superlist_early_in_huge_list() {
     let huge: Vec<u32> = (1..1_000_000).collect();
 
@@ -115,19 +138,19 @@ fn superlist_early_in_huge_list() {
 }
 
 #[test]
-#[ignore]
 fn recurring_values_sublist() {
     test_sublist(
         Comparison::Sublist,
-        &[1, 2, 1, 2, 3], &[1, 2, 3, 1, 2, 1, 2, 3, 2, 1]
+        &[1, 2, 1, 2, 3],
+        &[1, 2, 3, 1, 2, 1, 2, 3, 2, 1],
     );
 }
 
 #[test]
-#[ignore]
 fn recurring_values_unequal() {
     test_sublist(
         Comparison::Unequal,
-        &[1, 2, 1, 2, 3], &[1, 2, 3, 1, 2, 3, 2, 3, 2, 1]
+        &[1, 2, 1, 2, 3],
+        &[1, 2, 3, 1, 2, 3, 2, 3, 2, 1],
     );
 }
