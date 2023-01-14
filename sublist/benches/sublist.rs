37f7efe3a8c4135a1579d592966955f805d06d51
Evgeniia Vakarina <27793901+EvgeniiaVak@users.noreply.github.com>
2023-01-14 12:51:52 +0400

    refactor(sublist): bench methods with single length

--- sublist/benches/sublist.rs
+++ sublist/benches/sublist.rs
@@ -9,37 +9,28 @@ fn bench_sublist(c: &mut Criterion) {
     let mut group = c.benchmark_group("Sublist");
     group.sample_size(10);
     let methods = vec![Method::Rayon, Method::Sequential, Method::Threads];
+    let i = 100_000_000;
 
-    for i in [100_000_000] {
-        // random chars
-        let rand = |i| {
-            thread_rng()
-                .sample_iter(&Alphanumeric)
-                .take(i)
-                .map(char::from)
-                .collect::<Vec<char>>()
-        };
+    // random chars
+    let rand = |i| {
+        thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(i)
+            .map(char::from)
+            .collect::<Vec<char>>()
+    };
 
-        for method in methods.iter() {
-            let a = rand(i);
-            let b = rand(i / 4);
-            let method = method.clone();
-            group.bench_function(BenchmarkId::new(format!("{method:?}"), i), |bencher| {
-                bencher.iter(|| sublist(&a, &b, method))
-            });
-        }
+    for method in methods.iter() {
+        let a = rand(i);
+        let b = rand(i / 4);
+        let method = method.clone();
+        group.bench_function(
+            BenchmarkId::new("is_sublist", format!("{method:?}")),
+            |bencher| bencher.iter(|| sublist(&a, &b, method)),
+        );
     }
 
     group.finish();
-
-    // copy the result comparison image
-    match fs::copy(
-        "target/criterion/Sublist/report/violin.svg",
-        "benches/test_result.svg",
-    ) {
-        Ok(_) => println!("Copied the result comparison image to benches/test_result.svg"),
-        Err(e) => println!("Failed to copy the result comparison image: {}", e),
-    }
 }
 
 criterion_group!(
